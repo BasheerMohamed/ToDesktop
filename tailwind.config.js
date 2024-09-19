9dc6a2095dcb4ae1acb5e0f79ba238b967cf9152
BasheerMohamed <Basheer_Mohamed@proton.me>
2024-09-19 10:27:18 +0530

    #

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#3238f2'
+      },
       fontFamily: {
         display: ['Poppins', 'sans-serif'],
-        body: ['Inter', 'sans-serif'],
+        body: ['Inter', 'sans-serif']
       }
     },
   },
